2e17bc3a32c5f485eed589a9d2241a239bbebb2f
AceAttorneyMaster111 <NoahS2003@gmail.com>
2018-12-10 12:10:48 -0500

    Partial commit of new hardware features

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareBobAlexanderIII.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareBobAlexanderIII.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -31,10 +32,28 @@ public class HardwareBobAlexanderIII implements IBotHardware {
      * Right lift motor for the robot.
      */
     public DcMotor rightLift;
+    /**
+     * Arm motor for the robot.
+     */
+    public DcMotor arm;
+
+    // Constants
+    public static final double WHEEL_DIAMETER = 6;
+    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
 
     public HardwareMap hardwareMap;
+    private OpMode opMode;
 
     private ElapsedTime elapsedTime = new ElapsedTime();
+
+    /**
+     * Create an instance of Bob Alexander III.
+     * @author Noah Simon
+     * @param opMode Use <code>this</code>.
+     */
+    public HardwareBobAlexanderIII(OpMode opMode) {
+        this.opMode = opMode;
+    }
 
     @Override
     public void init(HardwareMap hardwareMap) {
@@ -43,13 +62,50 @@ public class HardwareBobAlexanderIII implements IBotHardware {
         rightDrive = register("motorRight", DcMotorSimple.Direction.REVERSE, DcMotor.RunMode.RUN_USING_ENCODER);
         leftLift = register("lifterLeft", DcMotorSimple.Direction.FORWARD, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightLift = register("lifterRight", DcMotorSimple.Direction.REVERSE, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm = register("arm", DcMotorSimple.Direction.FORWARD, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    /**
+     * A convenience function to configure a {@link DcMotor} in order to use it in an {@link com.qualcomm.robotcore.eventloop.opmode.OpMode}.
+     * @author Noah Simon
+     * @param name The name of the motor configured on the Android devices.
+     * @param direction The motor direction that should be set as positive.
+     * @param runMode How the motor should interpret commands from the OpMode.
+     * @return The motor created.
+     */
     private DcMotor register(String name, DcMotorSimple.Direction direction, DcMotor.RunMode runMode) {
         DcMotor motor = hardwareMap.get(DcMotor.class, name);
         motor.setDirection(direction);
         motor.setPower(0);
+        if (runMode == DcMotor.RunMode.RUN_USING_ENCODER) {
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Make sure encoders are reset
+        }
         motor.setMode(runMode);
         return motor;
+    }
+
+    @Override
+    @Deprecated
+    public void drive(double speed, double dist, double timeout) {
+        // FIXME: Not done yet.
+        // Code adapted from org.firstinspires.ftc.teamcode.EncoderAuto#encoderDrive
+        boolean stop = false;
+        if(opMode instanceof LinearOpMode && ((LinearOpMode) opMode).opModeIsActive()) {
+            double target = dist * 30 / WHEEL_CIRCUMFERENCE;
+            elapsedTime.reset();
+            leftDrive.setPower(0.5);
+            rightDrive.setPower(0.5);
+            while (((LinearOpMode) opMode).opModeIsActive() && elapsedTime.seconds() < timeout
+                    && !stop) {
+//                if (leftDrive.getCurrentPosition() >= )
+            }
+        } else {
+            opMode.telemetry.addData("Error", "Attempted to autodrive during teleop.");
+        }
+    }
+
+    @Override
+    public void turn(double speed, double angle, double timeout) {
+        // FIXME: Not done yet
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/IBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/IBotHardware.java
@@ -13,6 +13,28 @@ public interface IBotHardware {
      * (It's already defined by a superclass.)
      *
      * This function configures the robot to assign specific hardware elements to fields of this class.
+     * @param hardwareMap The hardware map defined by a superclass containing all of the hardware elements.
+     * @author Noah Simon
      */
     public void init(HardwareMap hardwareMap);
+
+    /**
+     * Use the drive motors to move the robot in a straight line.
+     *
+     * @author Noah Simon
+     * @param speed A value from -1 to 1, a higher absolute value meaning a higher speed.
+     * @param dist In inches, the distance the robot should travel.
+     * @param timeout In seconds, how long the robot should attempt to reach the target distance.
+     */
+    public void drive(double speed, double dist, double timeout);
+
+    /**
+     * Use the drive motors to turn the robot in place.
+     *
+     * @author Noah Simon
+     * @param speed A value from -1 to 1, a higher absolute value meaning a higher speed.
+     * @param angle In degrees, how far the robot should turn. A positive amount is counterclockwise.
+     * @param timeout In seconds, how long the robot should attempt to reach the target angle.
+     */
+    public void turn(double speed, double angle, double timeout);
 }
