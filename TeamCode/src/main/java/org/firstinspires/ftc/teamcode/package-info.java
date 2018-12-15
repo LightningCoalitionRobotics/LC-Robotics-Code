c08d9f525fa3b8b306599f9d5215d8874137deb0
AceAttorneyMaster111 <NoahS2003@gmail.com>
2018-12-15 13:03:12 -0500

    Generated javadoc
    (View at https://lightningcoalitionrobotics.github.io/LC-Robotics-Code/doc/javadoc)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/package-info.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/package-info.java
@@ -9,7 +9,7 @@
  *     <li>In that package, there are a bunch of sample classes that showcase different things you can code.
  *     Find one that suits your needs, and copy everything in the file.</li>
  *     <li>Rightclick (or two-finger click) on the folder "org.firstinspires.ftc.teamcode"</li>
- *     <li>New -> Java Class</li>
+ *     <li>New -&rt; Java Class</li>
  *     <li>Type whatever name you want to call your code, and click OK.</li>
  *     <li>Delete everything, and paste the sample into your file.</li>
  *     <li>Replace line 1 with this: <pre><code>package org.firstinspires.ftc.teamcode;</code></pre></li>
@@ -19,8 +19,8 @@
  *     <li>Once you feel like you have a good idea of how to use this code, tweak it to your own needs.
  *     (Change the numbers, add more movements, etc.) If you still aren't sure, come talk to Noah or Gabe.</li>
  *     <li>Save your work!</li>
- *     <li>From the top menu: VCS -> Git -> Add</li>
- *     <li>VCS -> Commit. In Commit Message, summarize what your code does.</li>
+ *     <li>From the top menu: VCS -&rt; Git -&rt; Add</li>
+ *     <li>VCS -&rt; Commit. In Commit Message, summarize what your code does.</li>
  *     <li>Click the arrow next to the Commit button, and click Commit and Push.</li>
  *     <li>Push, and you're done!</li>
  * </ol>
