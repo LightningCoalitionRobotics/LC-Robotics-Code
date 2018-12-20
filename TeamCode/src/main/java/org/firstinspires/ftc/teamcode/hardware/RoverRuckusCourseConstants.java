51c48b8a7223404a5123ec5f65303a446712f9be
AceAttorneyMaster111 <NoahS2003@gmail.com>
2018-12-20 16:07:51 -0500

    Added constants file.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RoverRuckusCourseConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+/**
+ * A series of useful constants relating to distances on the course.
+ *
+ * Please feel free to add more, but remember to document what it measures!
+ * @author Noah Simon
+ */
+public final class RoverRuckusCourseConstants {
+    /**
+     * The distance in inches from the lander wall to the center scoring element of the nearest set of 3.
+     */
+    public static final int LANDER_TO_MIDDlE_ELEMENT = 31;
+}
