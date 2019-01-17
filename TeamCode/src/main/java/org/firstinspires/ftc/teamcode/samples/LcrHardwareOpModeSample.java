3e7e11b65316299cdcbbef37735513e70c2b1b49
AceAttorneyMaster111 <NoahS2003@gmail.com>
2019-01-17 15:02:11 -0500

    Add sample file for HardwareBobAlexanderIII

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/LcrHardwareOpModeSample.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareBobAlexanderIII;
+
+@Disabled
+public class LcrHardwareOpModeSample extends LinearOpMode {
+    private static final double DRIVE_SPEED = 0.5;
+    private HardwareBobAlexanderIII robot = new HardwareBobAlexanderIII(this, DRIVE_SPEED);
+
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        // Things you can do:
+        robot.moveMotor(robot.lifter, DRIVE_SPEED, 2); // Extend stronkboi for two seconds.
+        robot.drive(30, 5); // Move thirty inches with a timeout of five seconds.
+        robot.turn(40, 5); // Turn forty degrees counterclockwise with a timeout of five seconds.
+        robot.autoSpin(1, 2); // Spin the front spinners inward for two seconds.
+        robot.drive(false, 10, 10); // Move backward ten inches with a timeout of ten seconds.
+    }
+}
