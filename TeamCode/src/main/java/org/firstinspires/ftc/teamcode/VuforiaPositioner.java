8a56a62e30f602ab3e420bbc7ac09e7479b3bdca
AceAttorneyMaster111 <NoahS2003@gmail.com>
2019-01-17 15:03:44 -0500

    Addedd vuforia positioner (Worked on with William at competition)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaPositioner.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
+public class VuforiaPositioner {
+    private VuforiaLocalizer vuforia;
+    public VuforiaPositioner(VuforiaLocalizer vuforia) {
+        this.vuforia = vuforia;
+    }
+
+//    public Position findLocation() {
+//
+//    }
+
+    public class Position {
+        public double x;
+        public double y;
+
+        public Position(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
