7f1be0b7f2a358e654b8702bbbddf11b0255c978
AceAttorneyMaster111 <NoahS2003@gmail.com>
2019-10-16 14:36:59 -0400

    Quick fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code201819/HardwareTestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code201819/HardwareTestAutonomous.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.code201819;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.hardware.HardwareBobAlexanderIII;
