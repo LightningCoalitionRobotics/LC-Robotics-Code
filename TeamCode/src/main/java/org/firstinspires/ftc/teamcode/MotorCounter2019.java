f8a62c857f03cb670f9829c1a04db5b11b917800
AceAttorneyMaster111 <NoahS2003@gmail.com>
2019-10-19 14:58:16 -0400

    Created motor counter class based off of last year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorCounter2019.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Hardware;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
+
+public class MotorCounter2019 extends OpMode {
+    private HardwareLilPanini robot = new HardwareLilPanini(this);
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+
+        robot.frontController.setMotorMode(1, DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontController.setMotorMode(2, DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rearController.setMotorMode(1, DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rearController.setMotorMode(2, DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            telemetry.addData("Exception",e);
+        }
+
+        robot.frontController.setMotorMode(1, DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontController.setMotorMode(2, DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rearController.setMotorMode(1, DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rearController.setMotorMode(2, DcMotor.RunMode.RUN_USING_ENCODER);
+
+        topLeftPos = robot.frontController.getMotorCurrentPosition(2);
+        topRightPos = robot.frontController.getMotorCurrentPosition(1);
+        bottomLeftPos = robot.frontController.getMotorCurrentPosition(1);
+        bottomRightPos = robot.rearController.getMotorCurrentPosition(2);
+    }
+
+    private int countsPerRevolutionTopLeft = 0;
+    private int countsPerRevolutionTopRight = 0;
+    private int countsPerRevolutionBottomLeft = 0;
+    private int countsPerRevolutionBottomRight = 0;
+
+    private int topLeftPos;
+    private int topRightPos;
+    private int bottomLeftPos;
+    private int bottomRightPos;
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            robot.frontController.setMotorPower(2, 0.1);
+            robot.frontController.setMotorPower(1, 0.1);
+            robot.rearController.setMotorPower(2, 0.1);
+            robot.rearController.setMotorPower(1, 0.1);
+        } else if (gamepad1.y) {
+            robot.frontController.setMotorPower(2, 0.1);
+            robot.frontController.setMotorPower(1, -0.1);
+            robot.rearController.setMotorPower(2, -0.1);
+            robot.rearController.setMotorPower(1, 0.1);
+        } else {
+            robot.frontController.setMotorPower(2, 0);
+            robot.frontController.setMotorPower(1, 0);
+            robot.rearController.setMotorPower(2, 0);
+            robot.rearController.setMotorPower(1, 0);
+        }
+
+        countsPerRevolutionTopLeft = robot.frontController.getMotorCurrentPosition(2) - topLeftPos;
+        countsPerRevolutionTopRight = robot.frontController.getMotorCurrentPosition(1) - topRightPos;
+        countsPerRevolutionBottomLeft = robot.rearController.getMotorCurrentPosition(1) - bottomLeftPos;
+        countsPerRevolutionBottomRight = robot.rearController.getMotorCurrentPosition(2) - bottomRightPos;
+
+        telemetry.addData("Current top left position:", robot.frontController.getMotorCurrentPosition(2));
+        telemetry.addData("Current top right position", robot.frontController.getMotorCurrentPosition(1));
+        telemetry.addData("Current bottom left position:", robot.rearController.getMotorCurrentPosition(1));
+        telemetry.addData("Current bottom right position", robot.rearController.getMotorCurrentPosition(2));
+
+        telemetry.addData("Counts per revolution (top left)", countsPerRevolutionTopLeft);
+        telemetry.addData("Counts per revolution (top right)", countsPerRevolutionTopRight);
+        telemetry.addData("Counts per revolution (bottom left)", countsPerRevolutionBottomLeft);
+        telemetry.addData("Counts per revolution (bottom right)", countsPerRevolutionBottomRight);
+        
+        telemetry.update();
+    }
+}
