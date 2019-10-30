ef3763e627120c41a0d36f16987d1b44a65848c5
AC <acunningham@keyschool.org>
2019-10-30 16:03:43 -0400

    I added the kicks.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrogloditeCave.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrogloditeCave.java
@@ -25,7 +25,32 @@ public class TrogloditeCave extends LinearOpMode {
         robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT,1, 8, 5 );
         sleep(20);
         robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT,1, 8, 5 );
-    }
+        sleep(20);
+        robot.frontController.setMotorPower(1, 1);
+        robot.rearController.setMotorPower(2, 1);
+        sleep(5);
+        robot.frontController.setMotorPower(1, -1);
+        robot.rearController.setMotorPower(2, -1);
+        sleep(5);
+        robot.frontController.setMotorPower(2, 1);
+        robot.rearController.setMotorPower(1, 1);
+        sleep(5);
+        robot.frontController.setMotorPower(2, -1);
+        robot.rearController.setMotorPower(1, -1);
+        sleep(5);
+        robot.frontController.setMotorPower(1, 1);
+        robot.rearController.setMotorPower(2, 1);
+        sleep(5);
+        robot.frontController.setMotorPower(1, -1);
+        robot.rearController.setMotorPower(2, -1);
+        sleep(5);
+        robot.frontController.setMotorPower(2, 1);
+        robot.rearController.setMotorPower(1, 1);
+        sleep(5);
+        robot.frontController.setMotorPower(2, -1);
+        robot.rearController.setMotorPower(1, -1);
+        sleep(5);
+            }
     @Override
     public void runOpMode(){
         robot.init(hardwareMap);
