88d2a9999a73185afcde9cd4ecc7c1095a91b19a
YoinkerDoinker <chris_cross_country@yahoo.com>
2019-11-04 15:33:32 -0500

    More work on the autonomous that should test all of our functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
 public class TestAutonomous extends LinearOpMode {
     private HardwareLilPanini robot = new HardwareLilPanini(this);
 
-    private void test() {
+    private void testdrive() {
         robot.drive(.5, 10, 60);    //test forwards
         sleep(2000);
         robot.drive(-0.5, 10, 60);  //test backwards
@@ -16,11 +16,25 @@ public class TestAutonomous extends LinearOpMode {
         robot.drive(1, 20, 5);      //test timeout
         sleep(2000);
     }
-
+    private void testturn() {
+        robot.turn(.5,30,60); //testing positive angle and positive speed
+        sleep(2000);
+        robot.turn(.5,-30,60); //test negative angle and positive speed
+        sleep(2000);
+        robot.turn(-.5,30,60); //test positive angle and negative speed
+        sleep(2000);
+        robot.turn(-.5,-30,60); //test negative angle and negative speed
+        sleep(2000);
+        robot.turn(1,0,1); //test timeout
+        sleep(2000);
+    }
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
         waitForStart();
-        test();
+        testdrive();
+        testturn();
+        // teststrafe();
+        // testturnangle();
     }
 }
