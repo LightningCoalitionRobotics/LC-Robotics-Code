d41ae2ba6649d4adbc1389f31e7509c79777271b
YoinkerDoinker <chris_cross_country@yahoo.com>
2019-11-05 15:06:02 -0500

    Created a new file as a sample for autonomous with detailed explanation of how to use ever function of the robot and coding examples. Not finished yet.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/Lcr2019AutonomousSample.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
+
+/**
+ * A sample Autonomous that shows you how to use HardwareLilPanini
+ * Helpful to look over, it will give you an overview of everything we can currently do with the robot and how to do it
+ *
+ * @author Chris
+ */
+
+@Disabled //This serves to tell the phone not to download it, if you actually are going to create an autonomous program do not include this
+@Autonomous(name="Lcr2019AutonomousSample", group="autonomous")
+public class Lcr2019AutonomousSample extends LinearOpMode { //This class extends (inherits) properties from LinearOpMode
+    private HardwareLilPanini robot = new HardwareLilPanini(this);
+
+    private void movements() { //Put what you want the robot to do here
+        //CODE
+
+    }
+
+    @Override // Let the code know that this will override anything conflicting in the class it inherited stuff from
+    public void runOpMode(){ //When the init button is pressed
+        robot.init(hardwareMap); //initializes the piece of code that tells it which motors are what
+        waitForStart(); //Wait until we press start on the phone (remember you have to press init and then start when you're ready
+        movements();
+    }
+}
