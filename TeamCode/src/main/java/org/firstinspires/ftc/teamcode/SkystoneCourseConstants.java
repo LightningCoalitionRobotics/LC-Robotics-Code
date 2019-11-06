999755975a8cee073654265584723c3d70de62b7
AceAttorneyMaster111 <NoahS2003@gmail.com>
2019-11-06 08:51:01 -0500

    Added course constants file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneCourseConstants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * A series of useful constants relating to distances on the course.
+ *
+ * Please feel free to add more, but remember to document what it measures!
+ * @author Noah Simon
+ */
+public final class SkystoneCourseConstants implements CourseConstants {
+}
