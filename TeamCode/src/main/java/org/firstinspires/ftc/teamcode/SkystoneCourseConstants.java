4c539740cffbd41026bb88283da1aa7399c189e2
YoinkerDoinker <chris_cross_country@yahoo.com>
2019-11-13 17:56:38 -0500

    Some course constants for autonomous programming.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneCourseConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneCourseConstants.java
@@ -7,4 +7,34 @@ package org.firstinspires.ftc.teamcode;
  * @author Noah Simon
  */
 public final class SkystoneCourseConstants implements CourseConstants {
+    /**
+     * The distance in inches from one side of the red beams to the other along the horizontal axis
+     */
+    public static final int RED_CLEARANCE_X = 47;
+
+    /**
+     * The distance in inches from one side of the yellow beams to the other along the horizontal axis
+     */
+    public static final int YELLOW_CLEARANCE_X = 44;
+
+    /**
+     * The distance in inches from the ground to the red beams
+     */
+    public static final double RED_CLEARANCE_Y = 14.5;
+
+    /**
+     * The distance in inches from the ground to the yellow beams
+     */
+    public static final double YELLOW_CLEARANCE_Y = 20.5;
+
+    /**
+     * The distance in inches of one block
+     */
+    public static final double ONE_BLOCK_ACROSS = 20;
+
+    /**
+     * The distance in inches of one diagonal of the square
+     */
+    public static final double ONE_BLOCK_DIAGONAL = 33;
+
 }
