c806b2f4a70e25320d0eddb7ff1d658797e5db63
YoinkerDoinker <chris_cross_country@yahoo.com>
2019-11-16 14:14:25 -0500

    Removed sleep from TestAutonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -18,50 +18,41 @@ public class TestAutonomous extends LinearOpMode {
     private void testdrive() {
         telemetry.addLine("Testing Drive function: Forwards at half speed");
         robot.drive(.5, 10, 60);    //test forwards
-        sleep(2000);
 
         telemetry.addLine("Testing Drive function: Backwards at half speed");
         robot.drive(-0.5, 10, 60);  //test backwards
-        sleep(2000);
 
         telemetry.addLine("Testing Drive function: Timeout after 5 seconds");
         robot.drive(1, 20, 5);      //test timeout
-        sleep(2000);
         //remember, the reason we don't test negative distance is because distance should never be negative
     }
     private void testturn() {
         telemetry.addLine("Testing Turn function: 30 degrees at half speed");
         robot.turn(.5,30,60); //testing positive angle and positive speed
-        sleep(2000);
 
         telemetry.addLine("Testing Turn function: -30 degrees  at half speed");
         robot.turn(.5,-30,60); //test negative angle and positive speed
-        sleep(2000);
 
         telemetry.addLine("Testing Turn function: Timeout after 5 seconds");
         robot.turn(1,700,5); //test timeout
-        sleep(2000);
         //remember, the reason we don't test negative speed is because speed should never be negative
     }
 
     private void teststrafe() {
         telemetry.addLine("Testing Strafe function: Left at half speed for 10 inches");
         robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT,.5,10,60);
-        sleep(2000);
 
         telemetry.addLine("Testing Strafe function: Right at half speed for 10 inches");
         robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,.5,10,60);
-        sleep(2000);
 
         telemetry.addLine("Testing Strafe function: Timeout after 5 seconds");
         robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,.5,40,5);
-        sleep(2000);
         //once again, speed and distance should always be positive so no need to test those
     }
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
-        waitForStart();
+        waitForStart( );
         testdrive();
         testturn();
         teststrafe();
