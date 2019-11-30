00ee62c5fb18f3842296021f32cf8a0754f25b03
JWeinstein42 <jweinstein@keyschool.org>
2019-11-30 13:47:45 -0500

    Since the robot could start in any of the four corners, I wrote two autonomous programs for if the robot starts next to the blocks. It should bulldoze the blocks and then turn and drive/ hopefully push some of the blocks under the bridge thingy?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SomethingRandom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SomethingRandom.java
@@ -20,20 +20,32 @@ public class SomethingRandom extends LinearOpMode {
        robot.drive(.93, 10, 3);
        robot.turn(5,2,9);
 
+//---------------------------------------------------------------------------------------
+
+        robot.drive(0.8,46, 50);
+        robot.turn(.8, 165, 50);
+        robot.drive(0.5, 24, 50);
+        robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT, 0.5, 30, 50);
+        robot.drive(0.5,37.5,50);
+        robot.turn(0.5,180,50);
+        robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT, 0.5,30, 50);
+        robot.drive(0.5,9, 50);
+        //Code for if robot starts in BlueStart2
 
 
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, 0.2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
-       robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT,0.7, .2, 1);
+        robot.drive(0.8,46, 50);
+        robot.turn(.8, 165, 50);
+        robot.drive(0.5, 24, 50);
+        robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT, 0.5, 30, 50);
+        robot.drive(0.5,37.5,50);
+        robot.turn(0.5,180,50);
+        robot.strafe(HardwareLilPanini.HorizontalDirection.RIGHT, 0.5,30, 50);
+        robot.drive(0.5,9, 50);
+        //Code for if robot starts in RedStart2
+
+
+
+
 
 
 
