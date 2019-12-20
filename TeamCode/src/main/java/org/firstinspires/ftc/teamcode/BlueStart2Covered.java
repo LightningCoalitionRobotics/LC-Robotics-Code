dba045a3f108ffeb99fbd211f40c79cfbf460982
Apollo1515 <eismc2rocks@gmail.com>
2019-12-20 10:42:40 -0800

    Updated Autonomous to set up Vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStart2Covered.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStart2Covered.java
@@ -7,10 +7,30 @@ import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
 
 @Autonomous(name="BlueStart2Covered", group = "autonomous")
 public class BlueStart2Covered extends LinearOpMode {
+
+    public static final String TAG = "Vuforia VuMark";
+
+    OpenGLMatrix lastLocation = null;
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    VuforiaLocalizer vuforia;
+
     private HardwareLilPanini robot = new HardwareLilPanini(this);
 
     @Override
     public void runOpMode() {
+        //Setting up Vuforia to find Skystone
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AZLuN0P/////AAABmXgtUaFkU0DIk56Jx+XytyKDs9/Ax2YiCDZkM76i5kOmHu1gIDWVNX4LS1fRmfnreaxfK3keo5gWF+Ot6tSDVDLTy3vCR3skPLoZmR+ZiOhiXHSucmBiO2GPPHfSvuhDK+1l4Mqc+ogxle0SlPJz3DhZz47DT07XBSvJaXFBDd/tHeIodQVb0ysOi0yRbUNQ7RkxftKt3lRCq/5JwS28/TiNTpE33psKj3rusF5LLyFB0XviowHrPdLObQEhuCbY0LUljVagijOJ6jvYciIZhRBK65fjDqFCsVkd9+d2waFMhC1JdZD2VnuCkblfdnWpd+EOkrzqCtCDf6bHSEdnzSnzc5jXuzFhNcjyMmdIY9S+";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+
+
         robot.init(hardwareMap);
         waitForStart();
 
