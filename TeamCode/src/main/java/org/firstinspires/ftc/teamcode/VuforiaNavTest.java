242c59dedbaeafd85fb279f91d1e78782230b68f
AceAttorneyMaster111 <NoahS2003@gmail.com>
2020-01-03 13:05:11 -0500

    Added some test stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNavTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNavTest.java
@@ -5,6 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name="VuforiaNavTest", group="Autonomous")
 public class VuforiaNavTest extends LcVuforiaOpMode {
     public void runTasks() {
-
+        if (searchForTrackables().size() > 0) {
+            telemetry.addData("Current position", getLastPosition());
+            telemetry.addData("Current orientation", getLastOrientation());
+        } else {
+            telemetry.addLine("No targets found.");
+        }
+        telemetry.update();
+        sleep(2000);
+        robot.drive(1, 10, 10);
     }
 }
