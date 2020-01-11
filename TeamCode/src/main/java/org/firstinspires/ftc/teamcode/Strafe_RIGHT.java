194a1fd2b27e65e2c134ad72bb36d02627541838
AC <acunningham@keyschool.org>
2020-01-11 13:44:58 -0500

    Made the Strafe_RIGHT program for autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Strafe_RIGHT.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
+
+@Autonomous(name = "Strafe_RIGHT", group = "autonomous")
+public class Strafe_RIGHT extends LinearOpMode {
+
+    HardwareLilPanini robot = new HardwareLilPanini(this);
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        waitForStart();
+        start();
+
+        robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT, 0.3, 15, 50);
+        robot.stop();
+        
+    }
+
+}
