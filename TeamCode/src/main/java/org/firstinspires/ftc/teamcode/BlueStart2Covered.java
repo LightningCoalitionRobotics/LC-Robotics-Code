3cb09e112c3a6f2651a9466c1c84a41b6c2550a7
AC <acunningham@keyschool.org>
2020-01-11 09:42:21 -0500

    Oops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStart2Covered.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStart2Covered.java
@@ -15,6 +15,8 @@ public class BlueStart2Covered extends LcVuforiaOpMode {
         robot.init(hardwareMap);
         waitForStart();
 
+        robot.extend(3, 50, 0.3);
+        robot.release(5);
         robot.drive(0.5, 29, 50);
 
         int frontRightTarget = robot.motorFrontRight.getCurrentPosition();
@@ -29,7 +31,8 @@ public class BlueStart2Covered extends LcVuforiaOpMode {
             robot.motorBackLeft.setPower(-0.5);
             robot.motorBackRight.setPower(0.5);
         }
-        //code to pick up block goes here
+        robot.extend(-3, 50, 0.3);
+        robot.grab(1);
 
         while (robot.motorFrontRight.getCurrentPosition() >= frontRightTarget || robot.motorFrontLeft.getCurrentPosition() <= frontLeftTarget || robot.motorBackLeft.getCurrentPosition() >= backLeftTarget || robot.motorBackRight.getCurrentPosition() <= backRightTarget) {
             robot.motorFrontRight.setPower(0.5);
@@ -39,7 +42,10 @@ public class BlueStart2Covered extends LcVuforiaOpMode {
         }
         robot.drive(-0.7, 25, 50);
         robot.turn(0.6, 90, 50);
-        robot.drive(0.7, 51, 50);
+        robot.drive(0.7, 49, 50);
+        robot.extend(5, 50);
+        robot.drive(0.5, 3, 50);
+        robot.release(1);
 
     }
 
