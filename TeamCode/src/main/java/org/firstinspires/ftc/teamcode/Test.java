0cc1bbfb5e982e9a9b2b2a314888a413a3766e6b
AC <acunningham@keyschool.org>
2020-01-20 11:49:38 -0500

    Made a short little test program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -5,20 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
 
-@Autonomous(name = "Strafe_RIGHT", group = "autonomous")
-public class Strafe_RIGHT extends LinearOpMode {
-
-    HardwareLilPanini robot = new HardwareLilPanini(this);
+@Autonomous(name="Test", group="autonomous")
+public class Test extends LinearOpMode {
+    private HardwareLilPanini robot = new HardwareLilPanini(this);
 
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
         waitForStart();
-        start();
 
-        robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT, 0.3, 15, 50);
-        robot.stop();
-
+        robot.strafe(HardwareLilPanini.HorizontalDirection.LEFT, 0.5, 5, 50);
+        robot.drive(0.5, 5, 50);
+        robot.turn(0.5, -30, 50);
     }
-
 }
