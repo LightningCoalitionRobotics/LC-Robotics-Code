f2f911f97c32c1ad7317f35d5e3c6629fe766873
JWeinstein42 <jweinstein@keyschool.org>
2020-02-08 12:22:38 -0500

    Fixed Program after running

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStart2Uncovered.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStart2Uncovered.java
@@ -28,20 +28,20 @@ public class BlueStart2Uncovered extends LcVuforiaOpMode {
         //finding number of counts before while loop
 
         while (!opModeIsActive() && isVisible(stoneTarget)) {
-            robot.motorFrontRight.setPower(-0.5);
-            robot.motorFrontLeft.setPower(0.5);
-            robot.motorBackLeft.setPower(-0.5);
-            robot.motorBackRight.setPower(0.5);
+            robot.motorFrontRight.setPower(-0.25);
+            robot.motorFrontLeft.setPower(0.25);
+            robot.motorBackLeft.setPower(-0.25);
+            robot.motorBackRight.setPower(0.25);
         }
 
         robot.extend(4, 69);
         robot.grab(1);
 
         while (opModeIsActive() && (robot.motorFrontRight.getCurrentPosition() >= frontRightTarget || robot.motorFrontLeft.getCurrentPosition() <= frontLeftTarget || robot.motorBackLeft.getCurrentPosition() >= backLeftTarget || robot.motorBackRight.getCurrentPosition() <= backRightTarget)) {
-            robot.motorFrontRight.setPower(0.5);
-            robot.motorFrontLeft.setPower(-0.5);
-            robot.motorBackLeft.setPower(0.5);
-            robot.motorBackRight.setPower(-0.5);
+            robot.motorFrontRight.setPower(0.25);
+            robot.motorFrontLeft.setPower(-0.25);
+            robot.motorBackLeft.setPower(0.25);
+            robot.motorBackRight.setPower(-0.25);
         }
         robot.drive(-0.7, 30, 50);
         robot.turn(0.6, 90, 50);
