962b81283df2646df071a951371e0d56276d46a7
JWeinstein42 <jweinstein@keyschool.org>
2020-02-08 10:40:53 -0500

    Switched Blue start Covered to Red and fixed errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStart2Covered.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStart2Covered.java
@@ -16,6 +16,53 @@ public class RedStart2Covered extends LcVuforiaOpMode{
         robot.init(hardwareMap);
         waitForStart();
 
+        robot.extend(3, 50, 0.3);
+        robot.release(5);
+        robot.drive(0.5, 29, 50);
+
+        int frontRightTarget = robot.motorFrontRight.getCurrentPosition();
+        int frontLeftTarget = robot.motorFrontLeft.getCurrentPosition();
+        int backLeftTarget = robot.motorBackLeft.getCurrentPosition();
+        int backRightTarget = robot.motorBackRight.getCurrentPosition();
+        //finding number of counts before while loop
+
+        while (opModeIsActive() && !isVisible(stoneTarget)) {
+            robot.motorFrontRight.setPower(0.5);
+            robot.motorFrontLeft.setPower(-0.5);
+            robot.motorBackLeft.setPower(0.5);
+            robot.motorBackRight.setPower(-0.5);
+        }
+        robot.extend(-3, 50, 0.3);
+        robot.grab(1);
+
+        while (opModeIsActive() && (robot.motorFrontRight.getCurrentPosition() >= frontRightTarget || robot.motorFrontLeft.getCurrentPosition() <= frontLeftTarget || robot.motorBackLeft.getCurrentPosition() >= backLeftTarget || robot.motorBackRight.getCurrentPosition() <= backRightTarget)) {
+            robot.motorFrontRight.setPower(-0.5);
+            robot.motorFrontLeft.setPower(0.5);
+            robot.motorBackLeft.setPower(-0.5);
+            robot.motorBackRight.setPower(0.5);
+        }
+        robot.drive(-0.7, 25, 50);
+        robot.turn(0.6, 90, 50);
+        robot.drive(0.7, 49, 50);
+
+        robot.extend(5, 50);
+        robot.drive(0.5, 3, 50);
+        robot.extend(-2, 50);
+        robot.release(1);
+
+    }
+
+}
+
+
+
+
+
+     /*@Override
+    public void runTasks() {
+        robot.init(hardwareMap);
+        waitForStart();
+
         robot.extend(HardwareLilPanini.EXTENSION_INCHES/4, 50);
         robot.drive(0.5, 29, 50);
 
