914313f6c2ff604b118ce4fdc48d5526b5f503da
JWeinstein42 <jweinstein@keyschool.org>
2021-12-13 15:16:12 -0500

    Test TeleOp to adjust and test game pads/joysticks.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareReginaldIII.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareReginaldIII.java
@@ -53,7 +53,6 @@ public class HardwareReginaldIII extends Robot {
 
     public DcMotor motorBackRight;
 
-    public DcMotor motorDrawerSlide;
 
     public DcMotor grabber;
 
@@ -74,7 +73,6 @@ public class HardwareReginaldIII extends Robot {
         motorFrontRight = registerMotor("motorFrontRight", DcMotorSimple.Direction.REVERSE, DcMotor.RunMode.RUN_WITHOUT_ENCODER); //this direction is reverse because the motor is backward, so to make it go forwards you (if you had this forwards) would have to set a negative speed
         motorBackLeft = registerMotor("motorRearLeft", DcMotorSimple.Direction.FORWARD, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorBackRight = registerMotor("motorRearRight", DcMotorSimple.Direction.REVERSE, DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Same problem as above with this motor
-        motorDrawerSlide = registerMotor("motorDrawerSlide", DcMotorSimple.Direction.REVERSE, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         grabber = registerMotor("grabber", DcMotorSimple.Direction.FORWARD, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         //arm2 = registerServo("arm2", 0.5f);
     }
