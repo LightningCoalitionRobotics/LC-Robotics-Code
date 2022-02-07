cbe7298d4d132dafe2023512262b689a48f0dd6d
AC <acunningham@keyschool.org>
2022-02-07 15:05:20 -0500

    Made some changes to HardwareCletus trying to fix the stupid drive function that doesn't work now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexsTeleOp.java
@@ -14,164 +14,86 @@ public class AlexsTeleOp extends OpMode {
     public void init() {
         robot.init(hardwareMap);
     }
+
     double speed = 0.5; // a double that stores a speed that can be increased or decreased using the dpad
-    double speedArm = 0.0;
-    /*  a double that stores the speed used when moving the arm with the y or a buttons
-        used to make sure that the arm doesn't stop halfway up
-        if (gamepad2.atRest()) would stop it halfway through, as the button is only pressed once
-        changing it to if(gamepad2.atRest() && speedArm == 0) will make sure that if the arm is moving because of y or a buttons, it will finish the movement before stopping
-    */
 
     public void loop() {
-        //movement gamepad: triggers for forward/backward, b/x for strafing left and right, right joystick for moving tangent, left joystick for turning, dpad for changing speed
-        //arm gamepad: y/a to move arm to max/min height, b/x to open and close claw, dpad for more precise height changes
-        double startTime = getRuntime();
-        telemetry.addLine("double startTime = " + startTime );
-        telemetry.addLine("double speedArm = " + speedArm );
-        telemetry.addLine("counts in order (fR, fL, bR, bL) is: " + robot.motorFrontRight.getCurrentPosition() + ", " + robot.motorFrontLeft.getCurrentPosition() + ", " + robot.motorBackRight.getCurrentPosition() + ", " + robot.motorBackLeft.getCurrentPosition());
-
-        if(gamepad1.dpad_up && speed <= 1){
+        if (gamepad1.dpad_up && speed <= 1) {
             speed *= 2;
-            telemetry.addLine("pad 1 dpad up pushed");
             telemetry.addLine("speed is now " + speed);
 
-        } else if(gamepad1.dpad_down && speed >= 0.25) {
+        } else if (gamepad1.dpad_down && speed >= 0.25) {
             speed /= 2;
-            telemetry.addLine("pad 1 dpad down  pushed");
             telemetry.addLine("speed is now " + speed);
 
         }
 
         //controls for the movement gamepad
-        if(gamepad1.atRest()){
-           robot.stopMotor();
+        if (gamepad1.atRest()) {
+            robot.stopMotor();
 
-        } else if(gamepad1.right_stick_y > 0) {
+        } else if (gamepad1.right_stick_y > 0) {
+            //drive forwards
             robot.motorBackLeft.setPower(speed);
             robot.motorBackRight.setPower(speed);
             robot.motorFrontLeft.setPower(speed);
             robot.motorFrontRight.setPower(speed);
 
-        } else if(gamepad1.right_stick_y < 0){
+        } else if (gamepad1.right_stick_y < 0) {
+            //drive backwards
             robot.motorBackLeft.setPower(-speed);
             robot.motorBackRight.setPower(-speed);
             robot.motorFrontLeft.setPower(-speed);
             robot.motorFrontRight.setPower(-speed);
 
-        /* } else if(gamepad1.left_trigger > 0.49) {
-            //moves forwards
-            robot.motorBackLeft.setPower(speed);
-            robot.motorBackRight.setPower(speed);
-            robot.motorFrontLeft.setPower(speed);
-            robot.motorFrontRight.setPower(speed);
-
-        } else if(gamepad1.right_trigger > 0.49){
-            //moves backwards
-            robot.motorBackLeft.setPower(-speed);
-            robot.motorBackRight.setPower(-speed);
-            robot.motorFrontLeft.setPower(-speed);
-            robot.motorFrontRight.setPower(-speed);
-
-       } else if(gamepad1.left_bumper){
-            //strafes left
-            robot.motorBackLeft.setPower(speed);
-            robot.motorBackRight.setPower(-speed);
-            robot.motorFrontLeft.setPower(-speed);
-            robot.motorFrontRight.setPower(speed);
-
-        } else if(gamepad1.right_bumper){
-            //strafe right
-            robot.motorBackLeft.setPower(-speed);
-            robot.motorBackRight.setPower(speed);
-            robot.motorFrontLeft.setPower(speed);
-            robot.motorFrontRight.setPower(-speed); */
-
-        } else if(gamepad1.left_stick_x > 0.25){
+        } else if (gamepad1.left_stick_x > 0.25) {
             //turn right
             robot.motorBackLeft.setPower(-speed);
             robot.motorBackRight.setPower(speed);
             robot.motorFrontLeft.setPower(-speed);
             robot.motorFrontRight.setPower(speed);
 
-        } else if(gamepad1.left_stick_x < -0.25){
+        } else if (gamepad1.left_stick_x < -0.25) {
             //turn left
             robot.motorBackLeft.setPower(speed);
             robot.motorBackRight.setPower(-speed);
             robot.motorFrontLeft.setPower(speed);
             robot.motorFrontRight.setPower(-speed);
 
-        } /*else if(gamepad1.right_stick_x != 0 && gamepad1.right_stick_y != 0){
-            double angle = Math.toDegrees(Math.atan(Math.abs(gamepad1.right_stick_y)/Math.abs(gamepad1.right_stick_x)));
-            int quadrant = 0;
-            if(gamepad1.right_stick_x > 0){
-                if(gamepad1.right_stick_y > 0){
-                    quadrant = 1;
-                } else if(gamepad1.right_stick_y < 0){
-                    quadrant = 4;
-                }
-            } else if (gamepad1.right_stick_x < 0){
-                if(gamepad1.right_stick_y > 0){
-                    quadrant = 2;
-                } else if(gamepad1.right_stick_y < 0){
-                    quadrant = 3;
-                }
-            }
-            robot.driveAngleIndefinite(angle, speed, quadrant);
-        }*/
+        }
+
         //controls for the arm gamepad
 
-        if(gamepad2.atRest() && speedArm == 0){
+        if (gamepad2.atRest()) {
             robot.arm.setPower(0);
 
-        }
+            if (gamepad2.dpad_up) {
+                robot.extend();
 
-        if(startTime >= 0.45 && speedArm != 0){
-            speedArm = 0;
+            } else if (gamepad2.dpad_down) {
+                robot.unextend();
 
-        } else if(startTime >= 0.65 && speedArm == 0){
-             robot.arm.setPower(0);
-        }
+            }
 
-        /*if(gamepad2.y){
-            resetStartTime();
-            speedArm = -0.75;
-            robot.arm.setPower(speedArm);
+            if (gamepad2.right_stick_y > 0) {
+                resetStartTime();
+                robot.arm.setPower(0.5);
 
-        } else if(gamepad2.a){
-            resetStartTime();
-            speedArm = 0.75;
-            robot.arm.setPower(speedArm);
+            } else if (gamepad2.right_stick_y < 0) {
+                resetStartTime();
+                robot.arm.setPower(0.5);
 
-        }*/
+            }
 
-        if(gamepad2.dpad_up){
-            robot.extend();
+            if (gamepad2.left_stick_y > 0) {
+                robot.grabber.setPosition(robot.grabber.getPosition() + 0.001);
 
-        } else if(gamepad2.dpad_down){
-            robot.unextend();
+            } else if (gamepad2.left_stick_y < 0) {
+                robot.grabber.setPosition(robot.grabber.getPosition() - 0.001);
 
-        }
-
-        if(gamepad2.right_stick_y > 0){
-            resetStartTime();
-            robot.arm.setDirection(DcMotorSimple.Direction.FORWARD);
-            robot.arm.setPower(0.5);
-
-        } else if(gamepad2.right_stick_y < 0){
-            resetStartTime();
-            robot.arm.setDirection(DcMotorSimple.Direction.REVERSE);
-            robot.arm.setPower(0.5);
-
-        }
-
-        if(gamepad2.left_stick_y > 0){
-            robot.grabber.setPosition(robot.grabber.getPosition() + 0.001);
-
-        } else if(gamepad2.left_stick_y < 0){
-            robot.grabber.setPosition(robot.grabber.getPosition() - 0.001);
+            }
 
         }
 
     }
-
 }
