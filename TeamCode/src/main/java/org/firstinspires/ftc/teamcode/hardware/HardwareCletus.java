c1dbe57c2d3a3b94fe23f28a6df84cd577850840
AC <acunningham@keyschool.org>
2022-02-07 15:02:13 -0500

    Made some changes to HardwareCletus trying to fix the stupid drive function that doesn't work now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareCletus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareCletus.java
@@ -73,7 +73,6 @@ public class HardwareCletus extends Robot {
     public void drive(double speed, double dist, double timeout) {
         int distInCounts = (int) (dist * COUNTS_PER_FORWARD_INCH); //convert distance from human inches to motor counts
 
-
         // Target count value for each motor given dist, calculated from current position in counts plus (or minus if going backwards) distance in counts
 
         int topRightTarget;
@@ -125,18 +124,23 @@ public class HardwareCletus extends Robot {
         stopMotor();
     }
 
+    /**
+     * @param speed A value from 0 to 1, a higher value meaning a higher speed.
+     * @param angle In degrees, how far the robot should turn. A positive amount is clockwise.
+     * @param timeout In seconds, how long the robot should attempt to reach the target angle.
+     */
     @Override
     public void turn(double speed, double angle, double timeout) {
         int angleInCounts = (int)(angle * COUNTS_PER_DEGREE);
         //changes the angle variable from degrees to counts
 
-        int topRightTarget = motorFrontRight.getCurrentPosition() + angleInCounts;
-        int topLeftTarget = motorFrontLeft.getCurrentPosition() - angleInCounts;
-        int bottomLeftTarget = motorBackLeft.getCurrentPosition() - angleInCounts;
-        int bottomRightTarget = motorBackRight.getCurrentPosition() + angleInCounts;
+        int topRightTarget = motorFrontRight.getCurrentPosition() - angleInCounts;
+        int topLeftTarget = motorFrontLeft.getCurrentPosition() + angleInCounts;
+        int bottomLeftTarget = motorBackLeft.getCurrentPosition() + angleInCounts;
+        int bottomRightTarget = motorBackRight.getCurrentPosition() - angleInCounts;
         //finds target number of counts for each motor
 
-        if (angle > 0) {
+        if (angle < 0) {
             motorFrontRight.setPower(speed);
             motorFrontLeft.setPower(-speed);
             motorBackLeft.setPower(-speed);
@@ -150,7 +154,7 @@ public class HardwareCletus extends Robot {
             //sets left motors to positive and right to negative for clockwise turn
         }
         while (((LinearOpMode) opMode).opModeIsActive() && elapsedTime.seconds() < timeout){ //while opmode active and timenout not reached
-            if (angle > 0){
+            if (angle < 0){
                 if (motorFrontRight.getCurrentPosition() >= topRightTarget || motorFrontLeft.getCurrentPosition() <= topLeftTarget || motorBackLeft.getCurrentPosition() <= bottomLeftTarget || motorBackRight.getCurrentPosition() >= bottomRightTarget) {
                     break;
                 }
@@ -166,72 +170,6 @@ public class HardwareCletus extends Robot {
 
     }
 
-    public void driveIndefinite(double speed){
-        motorFrontRight.setPower(speed); //set motors to speed
-        motorFrontLeft.setPower(speed);
-        motorBackRight.setPower(speed);
-        motorBackLeft.setPower(speed);
-    }
-
-    public void turnIndefinite(double speed){
-        motorFrontRight.setPower(-speed);
-        motorFrontLeft.setPower(speed);
-        motorBackLeft.setPower(speed);
-        motorBackRight.setPower(-speed);
-    }
-
-    public void strafeIndefinite(double speed){
-        motorFrontRight.setPower(-speed); //set motors to speed
-        motorFrontLeft.setPower(speed);
-        motorBackRight.setPower(speed);
-        motorBackLeft.setPower(-speed);
-    }
-
-    public void driveAngleIndefinite(double degrees, double speed, int quadrant){
-        double a; // larger movement vector
-        double b; // smaller movement vector
-
-        double tangent = Math.tan(degrees);
-
-        a = 1;
-        // tan ø = a+b / a-b
-        // tangent ø = 1+b/1-b
-        // tangent ø - tangent*b = 1+b
-        // tangent ø = (tangent+1)b + 1
-        // (tangent ø - 1)/(tangent ø + 1) = b
-        b = (tangent - 1)/(tangent + 1);
-
-        // Find targets for motors
-        if (degrees != 90) { //if degrees are not equal to 90, continue with driveAngle, if they are equal to 90, just use drive with speed and dist
-            if (quadrant == 1){
-                motorFrontRight.setPower(b * speed);
-                motorFrontLeft.setPower(a * speed);
-                motorBackLeft.setPower(b * speed);
-                motorBackRight.setPower(a * speed);
-
-            } else if (quadrant == 2){
-                motorFrontRight.setPower(a * speed);
-                motorFrontLeft.setPower(b * speed);
-                motorBackLeft.setPower(a * speed);
-                motorBackRight.setPower(b * speed);
-
-            } else if (quadrant == 3){
-                motorFrontRight.setPower(-b * speed);
-                motorFrontLeft.setPower(-a * speed);
-                motorBackLeft.setPower(-b * speed);
-                motorBackRight.setPower(-a * speed);
-
-            } else if (quadrant == 4){
-                motorFrontRight.setPower(-a * speed);
-                motorFrontLeft.setPower(-b * speed);
-                motorBackLeft.setPower(-a * speed);
-                motorBackRight.setPower(-b * speed);
-
-            }
-        } else { // if degrees == 90
-            driveIndefinite(speed); // this is because plugging 90 into driveAngle returns an angle (imagine a triangle with two 90 degree angles, obviously not possible), so we just use drive
-        }
-    }
     /**
      * Drive the robot at a particular angle.
      * @param degrees The angle at which to move the robot. Measured in degrees above the positive X axis. Do not type 90.
@@ -325,6 +263,7 @@ public class HardwareCletus extends Robot {
 
     public void liftArm(){
         arm.setPower(0.75);
+        //make it so that this stops eventually
     }
 
     // height of lvl 1 = 3.5 inches
@@ -335,13 +274,6 @@ public class HardwareCletus extends Robot {
         //min height for arm
 
     }
-
-
-
-
-
-
-
 
     /**
      * Strafe the robot left or right.
