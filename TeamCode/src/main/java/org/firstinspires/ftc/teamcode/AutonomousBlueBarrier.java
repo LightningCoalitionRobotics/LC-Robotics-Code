e8cef2a156e32cdd1c39579f340627298d044b4f
JWeinstein42 <jweinstein@keyschool.org>
2022-04-04 14:43:14 -0400

    Testing speed arm function values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueBarrier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueBarrier.java
@@ -1,17 +1,3 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.hardware.HardwareCletus;
-
-
-
-@Autonomous(name = "AutonomousBlueBarrier", group = "autonomous")
-public class  AutonomousBlueBarrier extends LinearOpMode {
-
-    public void runTasks();
 
 
 
@@ -20,6 +6,4 @@ public class  AutonomousBlueBarrier extends LinearOpMode {
 
 
 
-    }
 
-}
