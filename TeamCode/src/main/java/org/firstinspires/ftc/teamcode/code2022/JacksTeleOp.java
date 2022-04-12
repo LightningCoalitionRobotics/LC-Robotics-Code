990f316160ef57d11a707b40c5873cf254ec46d6
JWeinstein42 <jweinstein@keyschool.org>
2022-04-12 15:01:45 -0400

    Adding parenthesis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code2022/JacksTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code2022/JacksTeleOp.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.hardware.HardwareCletus;
 import java.util.Arrays;
 
 @TeleOp(name = "JacksTeleOp", group = "TeleOp")
-public abstract class JacksTeleOp extends OpMode {
+public class JacksTeleOp extends OpMode {
     private HardwareCletus robot = new HardwareCletus(this);
 
     public void init(){ robot.init(hardwareMap);}
@@ -19,14 +19,14 @@ public abstract class JacksTeleOp extends OpMode {
 
     public void loop() {
 //Speed setting --> Final Speed Variable will be fspeed
-        if (gamepad1.left_bumper && speed <=.95){  //for increasing or decreasing speed from starting speed of .5 on interval (0, 1)
+        if (gamepad1.left_bumper && speed <.95){  //for increasing or decreasing speed from starting speed of .5 on interval (0, 1)
            speed +=  0.05;
 
-        } else if(gamepad1.right_bumper && speed >=.05 ) {
-             speed = speed -= 0.05;
+        } else if(gamepad1.right_bumper && speed >.05 ) {
+             speed -= 0.05;
         }
 
-        telemetry. addLine("Speed is now" + speed);
+        telemetry.addLine("Speed is now" + speed);
 
 
         if(gamepad1.atRest()) {
