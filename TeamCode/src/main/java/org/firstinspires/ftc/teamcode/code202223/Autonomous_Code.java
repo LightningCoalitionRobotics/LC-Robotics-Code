ca5ab3bef6d74ebb4149aafda5b3b4c977216ea0
Ian H <ihedgecoth@keyschool.org>
2022-12-06 15:27:06 -0500

    Basic Atonomous movement in a Square and credits and discription

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/Autonomous_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/Autonomous_Code.java
@@ -15,5 +15,24 @@ public class Autonomous_Code extends LinearOpMode {
     public void runOpMode(){
         robot.init(hardwareMap);
         waitForStart();
+
+        robot.drive(0.5,15,50);
+        robot.turn(0.5,90,50);
+        robot.drive(0.5,15,50);
+        robot.turn(0.5,90,50);
+        robot.drive(0.5,15,50);
+        robot.turn(0.5,90,50);
+        robot.drive(0.5,15,50);
+        robot.turn(0.5,90,50);
+
     }
 }
+
+/*
+  One of the Autonomous Codes for the 2022-2023 School Year
+
+  If more components are added, please add them to this class
+
+  @author Ian Hedgecoth
+  @author Maya Kirshtein
+*/
