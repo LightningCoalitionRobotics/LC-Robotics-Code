c9e004f548afcf047a0fde4913a35f6e2dd92485
Ian H <ihedgecoth@keyschool.org>
2022-12-06 15:15:32 -0500

    Set Up Autonomous_Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/Autonomous_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/Autonomous_Code.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.code202223;
 
-public class Autonomous_Code {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareCletus;
+import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
+
+@Autonomous(name="First Atomomous", group = "autonomous")
+
+public class Autonomous_Code extends LinearOpMode {
+    private HardwareCletus robot = new HardwareCletus(this);
+    @Override
+    public void runOpMode(){
+        robot.init(hardwareMap);
+        waitForStart();
+    }
 }
