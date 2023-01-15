d85c691b52f3073b4fc79fa689763f79b732c765
ifeadeyosoye <iadeyosoye@keyschool.org>
2023-01-15 14:06:03 -0500

    edits

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/OpeningMoveRightSide.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.code202223;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.hardware.HardwareGoobus;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.hardware.HardwareLilPanini;
+
+@Autonomous(name="OpeningMoveLeftSide", group = "autonomous")
+
+
+public class OpeningMoveRightSide extends LinearOpMode {
+    private HardwareGoobus robot = new HardwareGoobus(this);
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        waitForStart();
+
+        robot.drive(.5, 84, 5);
+        robot.turn(.5, -90, 5);
+        robot.drive(.5, 96, 5);
+        robot.turn(.5, 90, 5);
+        robot.drive(.5, 29, 5);
+    }
+}
+
