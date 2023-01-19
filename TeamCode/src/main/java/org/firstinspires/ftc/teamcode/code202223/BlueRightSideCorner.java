717da27dcc307a4afdec7cdbb0cee1df138ed4d4
Ian Hedgecoth <ihedgecoth@keyschool.org>
2023-01-19 13:49:04 -0500

    Finished Blue side autonomous code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/BlueRightSideCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/BlueRightSideCorner.java
@@ -29,7 +29,7 @@ public class BlueRightSideCorner extends LinearOpMode {
         robot.lowerArm();
         robot.turn(.5, 45, 5);
         robot.drive(-.5, 24, 5);
-        robot.strafe(-.5, 12, 5);
+        robot.strafe(.5, 60, 5);
     }
 }
 
