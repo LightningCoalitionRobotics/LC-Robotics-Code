fcb3abe65a7f056ef053677a95877d7dd416586a
Ian Hedgecoth <ihedgecoth@keyschool.org>
2023-01-19 13:45:18 -0500

    Added Blue Left and right side corner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/BlueRightSideCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/BlueRightSideCorner.java
@@ -4,9 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.hardware.HardwareGoobus;
 
-@Autonomous(name="BlueLeftSideCorner", group = "autonomous")
+@Autonomous(name="BlueRightSideCorner", group = "autonomous")
 
-public class BlueLeftSideCorner extends LinearOpMode {
+
+public class BlueRightSideCorner extends LinearOpMode {
     private HardwareGoobus robot = new HardwareGoobus(this);
 
     @Override
@@ -14,21 +15,21 @@ public class BlueLeftSideCorner extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
 
-        //from origin of (36,9) or (108,135)
+
         robot.drive(.5, 3,  5);
-        robot.turn(.5, 90, 5);
-        robot.drive(.5, 24, 5);
         robot.turn(.5, -90, 5);
         robot.drive(.5, 24, 5);
-        robot.turn(.5, 45, 5);
+        robot.turn(.5, 90, 5);
+        robot.drive(.5, 24, 5);
+        robot.turn(.5, -45, 5);
         robot.liftArm();
         robot.drive(.5, 11.97, 5);
         robot.open();
         robot.drive(-.5, 11.97, 5);
         robot.lowerArm();
-        robot.turn(.5, -45, 5);
-        robot.strafe(.5, 84, 5);
-        robot.drive(-.5,36,5);
-
+        robot.turn(.5, 45, 5);
+        robot.drive(-.5, 24, 5);
+        robot.strafe(-.5, 12, 5);
     }
 }
+
