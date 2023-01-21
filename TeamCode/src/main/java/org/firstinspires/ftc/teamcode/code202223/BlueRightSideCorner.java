404ff01508620788568bb03a278fe528174ba4ca
Jeff D <jdowell55@gmail.com>
2023-01-21 11:12:37 -0500

    Commenting out calls to LiftLowerArm at competition - arm motor directions may not be matching up with encoder signs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/BlueRightSideCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code202223/BlueRightSideCorner.java
@@ -16,18 +16,18 @@ public class BlueRightSideCorner extends LinearOpMode {
         waitForStart();
 
 
-        robot.LiftLowerArm(.5, 4, 5);
+//        robot.LiftLowerArm(.5, 4, 5);
         robot.drive(.5, 3,  5);
         robot.turn(.5, -90, 5);
         robot.drive(.5, 24, 5);
         robot.turn(.5, 90, 5);
         robot.drive(.5, 24, 5);
         robot.turn(.5, -45, 5);
-        robot.LiftLowerArm(.5, 30, 5);
+//        robot.LiftLowerArm(.5, 30, 5);
         robot.drive(.5, 11.97, 5);
         robot.open();
         robot.drive(-.5, 11.97, 5);
-        robot.LiftLowerArm(-.5, 34, 5);
+//        robot.LiftLowerArm(-.5, 34, 5);
         robot.turn(.5, 45, 5);
         robot.strafe(.5, 60, 5);
         robot.drive(-.5, 24, 5);
